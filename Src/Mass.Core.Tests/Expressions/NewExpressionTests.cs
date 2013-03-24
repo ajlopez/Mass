08faa296ae97e8f5bb2392f0d4b43fa14b7cd615
ajlopez <webmaster@ajlopez.com>
2013-03-24 11:50:18 -0300

    NewExpression with first test

---
+++ Src/Mass.Core.Tests/Expressions/NewExpressionTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Mass.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Mass.Core.Expressions;
+    using Mass.Core.Language;
+
+    [TestClass]
+    public class NewExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateNewExpression()
+        {
+            DefinedClass dclass = new DefinedClass("Dog");
+            NewExpression expr = new NewExpression(new ConstantExpression(dclass), new IExpression[] { });
+
+            var result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(DynamicObject));
+
+            var obj = (DynamicObject)result;
+
+            Assert.AreSame(dclass, obj.Class);
+        }
+    }
+}
---
+++ Src/Mass.Core/Expressions/NewExpression.cs
@@ -0,0 +1,27 @@
+﻿namespace Mass.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Functions;
+    using Mass.Core.Language;
+
+    public class NewExpression : IExpression
+    {
+        private IExpression expression;
+        private IList<IExpression> arguments;
+
+        public NewExpression(IExpression expression, IList<IExpression> arguments)
+        {
+            this.expression = expression;
+            this.arguments = arguments;
+        }
+
+        public object Evaluate(Context context)
+        {
+            DefinedClass dclass = (DefinedClass)this.expression.Evaluate(context);
+            return dclass.CreateInstance();
+        }
+    }
+}
