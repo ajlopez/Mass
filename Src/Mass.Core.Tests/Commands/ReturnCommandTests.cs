f38620af9c0a6246febbad855f30e551b49844bb
ajlopez <webmaster@ajlopez.com>
2013-03-27 09:00:16 -0300

    ReturnCommand first tests

---
+++ Src/Mass.Core.Tests/Commands/ReturnCommandTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Mass.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ReturnCommandTests
+    {
+        [TestMethod]
+        public void ExecuteReturnCommand()
+        {
+            Context context = new Context();
+            ReturnCommand cmd = new ReturnCommand(new ConstantExpression(1));
+
+            Assert.AreEqual(1, cmd.Execute(context));
+            Assert.IsTrue(context.HasReturn());
+            Assert.AreEqual(1, context.GetReturn());
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ReturnCommand cmd1 = new ReturnCommand(new ConstantExpression(1));
+            ReturnCommand cmd2 = new ReturnCommand(new ConstantExpression(2));
+            ReturnCommand cmd3 = new ReturnCommand(new ConstantExpression(1));
+
+            Assert.IsTrue(cmd1.Equals(cmd3));
+            Assert.IsTrue(cmd3.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd3.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+        }
+    }
+}
---
+++ Src/Mass.Core/Commands/ReturnCommand.cs
@@ -0,0 +1,47 @@
+﻿namespace Mass.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+
+    public class ReturnCommand : ICommand
+    {
+        private static int hashcode = typeof(ReturnCommand).GetHashCode();
+
+        private IExpression expression;
+
+        public ReturnCommand(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public object Execute(Context context)
+        {
+            object value = this.expression.Evaluate(context);
+            context.SetReturn(value);
+            return value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ReturnCommand)
+            {
+                var expr = (ReturnCommand)obj;
+
+                return this.expression.Equals(expr.expression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.expression.GetHashCode() + hashcode;
+        }
+    }
+}
