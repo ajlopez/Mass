922c3dc2746a42d191669fc64394dd3431596cd7
ajlopez <webmaster@ajlopez.com>
2013-03-28 11:37:48 -0300

    ForEachCommand with first tests

---
+++ Src/Mass.Core.Tests/Commands/ForEachCommandTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Mass.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ForEachCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleForEach()
+        {
+            ForEachCommand cmd = new ForEachCommand("k", new ConstantExpression(new int[] { 1, 2, 3 }), new AssignCommand("a", new BinaryArithmeticExpression(new NameExpression("a"), new NameExpression("k"), ArithmeticOperator.Add)));
+            Context context = new Context();
+            Assert.IsNull(cmd.Execute(context));
+            Assert.AreEqual(6, context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ForEachCommand cmd1 = new ForEachCommand("k", new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+            ForEachCommand cmd2 = new ForEachCommand("k", new ConstantExpression(2), new AssignCommand("one", new ConstantExpression(1)));
+            ForEachCommand cmd3 = new ForEachCommand("q", new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(2)));
+            ForEachCommand cmd4 = new ForEachCommand("k", new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+        }
+    }
+}
---
+++ Src/Mass.Core/Commands/ForEachCommand.cs
@@ -0,0 +1,58 @@
+﻿namespace Mass.Core.Commands
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+
+    public class ForEachCommand : ICommand
+    {
+        private static int hashcode = typeof(ForEachCommand).GetHashCode();
+
+        private string name;
+        private IExpression expression;
+        private ICommand command;
+
+        public ForEachCommand(string name, IExpression expression, ICommand command)
+        {
+            this.name = name;
+            this.expression = expression;
+            this.command = command;
+        }
+
+        public object Execute(Context context)
+        {
+            var values = (IEnumerable)this.expression.Evaluate(context);
+
+            foreach (var value in values)
+            {
+                context.SetValue(this.name, value);
+                this.command.Execute(context);
+            }
+
+            return null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ForEachCommand)
+            {
+                var cmd = (ForEachCommand)obj;
+
+                return this.name.Equals(cmd.name) && this.expression.Equals(cmd.expression) && this.command.Equals(cmd.command);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() + this.expression.GetHashCode() + this.command.GetHashCode() + hashcode;
+        }
+    }
+}
