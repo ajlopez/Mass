2bda18c726de198cc7d045d0e31dd1981863c545
ajlopez <webmaster@ajlopez.com>
2013-03-29 11:10:22 -0300

    For command first tests

---
+++ Src/Mass.Core.Tests/Commands/ForCommandTests.cs
@@ -0,0 +1,41 @@
+﻿namespace Mass.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ForCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleFor()
+        {
+            ForCommand cmd = new ForCommand("k", new ConstantExpression(1), new ConstantExpression(3), new ConstantExpression(1), new AssignCommand("a", new BinaryArithmeticExpression(new NameExpression("a"), new NameExpression("k"), ArithmeticOperator.Add)));
+            Context context = new Context();
+            Assert.IsNull(cmd.Execute(context));
+            Assert.AreEqual(6, context.GetValue("a"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ForCommand cmd1 = new ForCommand("k", new ConstantExpression(1), new ConstantExpression(3), new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+            ForCommand cmd2 = new ForCommand("k", new ConstantExpression(2), new ConstantExpression(3), new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+            ForCommand cmd3 = new ForCommand("q", new ConstantExpression(1), new ConstantExpression(3), new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(2)));
+            ForCommand cmd4 = new ForCommand("k", new ConstantExpression(1), new ConstantExpression(3), new ConstantExpression(1), new AssignCommand("one", new ConstantExpression(1)));
+
+            Assert.IsTrue(cmd1.Equals(cmd4));
+            Assert.IsTrue(cmd4.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd4.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd1.Equals(cmd3));
+        }
+    }
+}
---
+++ Src/Mass.Core/Commands/ForCommand.cs
@@ -0,0 +1,64 @@
+﻿namespace Mass.Core.Commands
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+
+    public class ForCommand : ICommand
+    {
+        private static int hashcode = typeof(ForEachCommand).GetHashCode();
+
+        private string name;
+        private IExpression fromexpression;
+        private IExpression toexpression;
+        private IExpression stepexpression;
+        private ICommand command;
+
+        public ForCommand(string name, IExpression fromexpression, IExpression toexpression, IExpression stepexpression, ICommand command)
+        {
+            this.name = name;
+            this.fromexpression = fromexpression;
+            this.toexpression = toexpression;
+            this.stepexpression = stepexpression;
+            this.command = command;
+        }
+
+        public object Execute(Context context)
+        {
+            var from = (int)this.fromexpression.Evaluate(context);
+            var to = (int)this.toexpression.Evaluate(context);
+            var step = (int)this.stepexpression.Evaluate(context);
+
+            for (int k = from; k <= to; k += step)
+            {
+                context.SetValue(this.name, k);
+                this.command.Execute(context);
+            }
+
+            return null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ForCommand)
+            {
+                var cmd = (ForCommand)obj;
+
+                return this.name.Equals(cmd.name) && this.fromexpression.Equals(cmd.fromexpression) && this.toexpression.Equals(cmd.toexpression) && this.stepexpression.Equals(cmd.stepexpression) && this.command.Equals(cmd.command);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() + this.fromexpression.GetHashCode() + this.toexpression.GetHashCode() + this.stepexpression.GetHashCode() + this.command.GetHashCode() + hashcode;
+        }
+    }
+}
