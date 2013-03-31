4ceb96d05dc111af1cf1fded4f761b97edb2185d
ajlopez <webmaster@ajlopez.com>
2013-03-31 09:46:21 -0300

    Function expression first tests

---
+++ Src/Mass.Core.Tests/Expressions/FunctionExpressionTests.cs
@@ -0,0 +1,62 @@
+﻿namespace Mass.Core.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Mass.Core.Expressions;
+    using Mass.Core.Compiler;
+    using Mass.Core.Functions;
+    using Mass.Core.Commands;
+
+    [TestClass]
+    public class FunctionExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateFunctionExpression()
+        {
+            Parser parser = new Parser("a+b");
+            FunctionExpression expr = new FunctionExpression(new string[] { "a", "b" }, parser.ParseCommand());
+
+            var result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(IFunction));
+
+            var function = (IFunction)result;
+
+            Assert.AreEqual(3, function.Apply(new object[] { 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ICommand body1 = (new Parser("a+b")).ParseCommand();
+            ICommand body2 = (new Parser("a+c")).ParseCommand();
+            IList<string> parameters1 = new string[] { "a", "b" };
+            IList<string> parameters2 = new string[] { "a", "c" };
+            IList<string> parameters3 = new string[] { "a" };
+
+            IExpression expr1 = new FunctionExpression(parameters1, body1);
+            IExpression expr2 = new FunctionExpression(parameters1, body2);
+            IExpression expr3 = new FunctionExpression(parameters2, body1);
+            IExpression expr4 = new FunctionExpression(parameters3, body1);
+            IExpression expr5 = new FunctionExpression(parameters1, body1);
+
+            Assert.IsTrue(expr1.Equals(expr5));
+            Assert.IsTrue(expr5.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr5.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals(123));
+
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr3));
+            Assert.IsFalse(expr3.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr4));
+            Assert.IsFalse(expr4.Equals(expr1));
+        }
+    }
+}
---
+++ Src/Mass.Core/Expressions/FunctionExpression.cs
@@ -0,0 +1,61 @@
+﻿namespace Mass.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Functions;
+
+    public class FunctionExpression : IExpression
+    {
+        private IList<string> parameters;
+        private ICommand body;
+
+        public FunctionExpression(IList<string> parameters, ICommand body)
+        {
+            this.parameters = parameters;
+            this.body = body;
+        }
+
+        public object Evaluate(Context context)
+        {
+            return new DefinedFunction(this.body, this.parameters, context);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is FunctionExpression)
+            {
+                var expr = (FunctionExpression)obj;
+
+                if (!this.body.Equals(expr.body))
+                    return false;
+
+                if (this.parameters.Count != expr.parameters.Count)
+                    return false;
+
+                for (var k = 0; k < this.parameters.Count; k++)
+                    if (!this.parameters[k].Equals(expr.parameters[k]))
+                        return false;
+
+                return true;
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int result = this.body.GetHashCode();
+
+            foreach (var argument in this.parameters)
+                result += parameters.GetHashCode();
+
+            return result;
+        }
+    }
+}
