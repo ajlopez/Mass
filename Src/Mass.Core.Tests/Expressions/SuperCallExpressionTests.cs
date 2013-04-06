e853d980608d7b42b726fc5193f412c5a46bb8d9
ajlopez <webmaster@ajlopez.com>
2013-04-06 18:54:26 -0300

    SuperCallExpression with first tests

---
+++ Src/Mass.Core.Tests/Expressions/SuperCallExpressionTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Mass.Core.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+    using Mass.Core.Functions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Mass.Core.Language;
+
+    [TestClass]
+    public class SuperCallExpressionTests
+    {
+        [TestMethod]
+        public void CallSuperinitialize()
+        {
+            Machine machine = new Machine();
+            var dobj = (DynamicObject)machine.ExecuteText("class Animal\ndefine initialize(name)\nself.name=name\nend\nend\nclass Dog extends Animal\nend\nnew Dog(\"Foo\")");
+            SuperCallExpression expr = new SuperCallExpression(new IExpression[] { new ConstantExpression("Nero") });
+            Context context = new Context(machine.RootContext);
+            context.Set("self", dobj);
+
+            expr.Evaluate(context);
+
+            Assert.AreEqual("Nero", dobj.Get("name"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            SuperCallExpression expr1 = new SuperCallExpression(new IExpression[] { new ConstantExpression(1) });
+            SuperCallExpression expr2 = new SuperCallExpression(new IExpression[] { new ConstantExpression(2) });
+            SuperCallExpression expr3 = new SuperCallExpression(new IExpression[] { new ConstantExpression(2), new ConstantExpression(3) });
+            SuperCallExpression expr4 = new SuperCallExpression(new IExpression[] { new ConstantExpression(1) });
+
+            Assert.IsTrue(expr1.Equals(expr4));
+            Assert.IsTrue(expr4.Equals(expr1));
+            Assert.AreEqual(expr1.GetHashCode(), expr4.GetHashCode());
+
+            Assert.IsFalse(expr1.Equals(null));
+            Assert.IsFalse(expr1.Equals(123));
+
+            Assert.IsFalse(expr1.Equals(expr2));
+            Assert.IsFalse(expr2.Equals(expr1));
+            Assert.IsFalse(expr1.Equals(expr3));
+            Assert.IsFalse(expr3.Equals(expr1));
+        }
+    }
+}
---
+++ Src/Mass.Core/Expressions/SuperCallExpression.cs
@@ -0,0 +1,73 @@
+﻿namespace Mass.Core.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Functions;
+    using Mass.Core.Language;
+
+    public class SuperCallExpression : IExpression
+    {
+        private static int hashcode = typeof(SuperCallExpression).GetHashCode();
+
+        private IList<IExpression> arguments;
+
+        public SuperCallExpression(IList<IExpression> arguments)
+        {
+            this.arguments = arguments;
+        }
+
+        public object Evaluate(Context context)
+        {
+            IList<object> values = new List<object>();
+
+            foreach (var argument in this.arguments)
+                values.Add(argument.Evaluate(context));
+
+            DynamicObject dobj = (DynamicObject)context.Get("self");
+
+            if (dobj.Class == null || dobj.Class.Superclass == null)
+                return null;
+
+            IFunction initialize = dobj.Class.Superclass.GetInstanceMethod("initialize");
+
+            if (initialize == null)
+                return null;
+
+            return initialize.Apply(dobj, values);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is SuperCallExpression)
+            {
+                var expr = (SuperCallExpression)obj;
+
+                if (this.arguments.Count != expr.arguments.Count)
+                    return false;
+
+                for (var k = 0; k < this.arguments.Count; k++)
+                    if (!this.arguments[k].Equals(expr.arguments[k]))
+                        return false;
+
+                return true;
+            }
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int result = hashcode;
+
+            foreach (var argument in this.arguments)
+                result += argument.GetHashCode();
+
+            return result;
+        }
+    }
+}
