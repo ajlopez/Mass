0d123f999de3735c2acdf5e32942704c93a76cfa
ajlopez <webmaster@ajlopez.com>
2013-05-29 07:40:50 -0300

    Try Command, first steps

---
+++ Src/Mass.Core.Tests/Commands/TryCommandTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Mass.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Compiler;
+    using Mass.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TryCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleTry()
+        {
+            Parser cmdparser = new Parser("a = a + 1");
+            ICommand body = cmdparser.ParseCommand();
+
+            Context context = new Context();
+            context.Set("a", 1);
+
+            TryCommand cmd = new TryCommand(body);
+
+            Assert.IsNull(cmd.Execute(context));
+
+            Assert.AreEqual(2, context.Get("a"));
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            TryCommand cmd1 = new TryCommand(new AssignCommand("one", new ConstantExpression(1)));
+            TryCommand cmd2 = new TryCommand(new AssignCommand("one", new ConstantExpression(2)));
+            TryCommand cmd3 = new TryCommand(new AssignCommand("one", new ConstantExpression(1)));
+
+            Assert.IsTrue(cmd1.Equals(cmd3));
+            Assert.IsTrue(cmd3.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd3.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd2.Equals(cmd1));
+        }
+    }
+}
---
+++ Src/Mass.Core/Commands/TryCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace Mass.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+
+    public class TryCommand : ICommand
+    {
+        private static int hashcode = typeof(TryCommand).GetHashCode();
+
+        private ICommand command;
+
+        public TryCommand(ICommand command)
+        {
+            this.command = command;
+        }
+
+        public object Execute(Context context)
+        {
+            this.command.Execute(context);
+
+            if (context.HasReturnValue())
+                return context.GetReturnValue();
+
+            return null;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is TryCommand)
+            {
+                var cmd = (TryCommand)obj;
+
+                return this.command.Equals(cmd.command);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.command.GetHashCode() + hashcode;
+        }
+    }
+}
