25f12af4529d0067b889668c8b53355b7855a52c
ajlopez <webmaster@ajlopez.com>
2013-06-20 17:45:28 -0300

    Throw command with first tests

---
+++ Src/Mass.Core.Tests/Commands/ThrowCommandTests.cs
@@ -0,0 +1,65 @@
+﻿namespace Mass.Core.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Commands;
+    using Mass.Core.Compiler;
+    using Mass.Core.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ThrowCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleThrow()
+        {
+            ThrowCommand cmd = new ThrowCommand(new ConstantExpression("Error"));
+
+            try
+            {
+                cmd.Execute(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Error", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ExecuteThrowException()
+        {
+            ThrowCommand cmd = new ThrowCommand(new ConstantExpression(new InvalidOperationException("Error")));
+
+            try
+            {
+                cmd.Execute(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidOperationException));
+                Assert.AreEqual("Error", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Equals()
+        {
+            ThrowCommand cmd1 = new ThrowCommand(new ConstantExpression(1));
+            ThrowCommand cmd2 = new ThrowCommand(new ConstantExpression(2));
+            ThrowCommand cmd3 = new ThrowCommand(new ConstantExpression(1));
+
+            Assert.IsTrue(cmd1.Equals(cmd3));
+            Assert.IsTrue(cmd3.Equals(cmd1));
+            Assert.AreEqual(cmd1.GetHashCode(), cmd3.GetHashCode());
+
+            Assert.IsFalse(cmd1.Equals(null));
+            Assert.IsFalse(cmd1.Equals(123));
+            Assert.IsFalse(cmd1.Equals(cmd2));
+            Assert.IsFalse(cmd2.Equals(cmd1));
+        }
+    }
+}
---
+++ Src/Mass.Core/Commands/ThrowCommand.cs
@@ -0,0 +1,50 @@
+﻿namespace Mass.Core.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Mass.Core.Expressions;
+
+    public class ThrowCommand : ICommand
+    {
+        private static int hashcode = typeof(ThrowCommand).GetHashCode();
+
+        private IExpression expression;
+
+        public ThrowCommand(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public object Execute(Context context)
+        {
+            var result = this.expression.Evaluate(context);
+
+            if (result is Exception)
+                throw (Exception)result;
+
+            throw new Exception(result.ToString());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (obj is ThrowCommand)
+            {
+                var cmd = (ThrowCommand)obj;
+
+                return this.expression.Equals(cmd.expression);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.expression.GetHashCode() + hashcode;
+        }
+    }
+}
